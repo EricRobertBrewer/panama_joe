e01069b475dee76df48f10cf4b4b1c190ea11dbc
Eric Brewer <u1082228@umail.utah.edu>
2023-11-21 12:23:45 -0700

    Add wrapper. Add 's' to save.

--- panama_joe/record_demo.py
+++ panama_joe/record_demo.py
@@ -6,6 +6,8 @@ import pygame
 from gymnasium.utils import play
 from shimmy.atari_env import AtariEnv
 
+from panama_joe.wrappers import AtariDemo
+
 KEY_TO_MEANING = {
     tuple(): 'NOOP',
     (pygame.K_SPACE,): 'FIRE',
@@ -24,7 +26,8 @@ KEY_TO_MEANING = {
     (pygame.K_UP, pygame.K_RIGHT, pygame.K_SPACE): 'UPRIGHTFIRE',
     (pygame.K_UP, pygame.K_LEFT, pygame.K_SPACE): 'UPLEFTFIRE',
     (pygame.K_DOWN, pygame.K_RIGHT, pygame.K_SPACE): 'DOWNRIGHTFIRE',
-    (pygame.K_DOWN, pygame.K_LEFT, pygame.K_SPACE): 'DOWNLEFTFIRE'
+    (pygame.K_DOWN, pygame.K_LEFT, pygame.K_SPACE): 'DOWNLEFTFIRE',
+    (pygame.K_s,): 'SAVE'
 }
 
 
@@ -44,37 +47,41 @@ def main():
                         default=3.0,
                         type=float,
                         help='Ratio to render frame from the default screen size.')
+    parser.add_argument('--obs_type',
+                        default='ram',
+                        choices=('ram', 'rgb'),
+                        help='Observation type.')
+    parser.add_argument('--frameskip',
+                        default=1,
+                        type=int,
+                        help='Frames to skip.')
+    parser.add_argument('--repeat_action_probability',
+                        default=0.0,
+                        type=float,
+                        help='Stickiness.')
 
     args = parser.parse_args()
     kwargs = dict(vars(args))
     record_demo(**kwargs)
 
 
-def record_demo(env_id, fps=30, zoom=3.0):
+def record_demo(env_id, fps=30, zoom=3.0, obs_type='ram', frameskip=1, repeat_action_probability=0.0):
     pygame.init()
     pygame.display.set_caption(f'Recording demonstration for {env_id}')
 
-    env = gym.make(env_id, render_mode='rgb_array')
-    fd = open('a.txt', 'wb')
-    obss = list()
+    env = AtariDemo(gym.make(env_id, obs_type=obs_type, frameskip=frameskip, repeat_action_probability=repeat_action_probability, render_mode='rgb_array'))
 
     def callback(obs_t_, obs_tp1_, action_, rew_, terminated_, truncated_, info_):
         if isinstance(obs_t_, tuple):  # Very first callback comes with extra `info` object.
             obs_t_ = obs_t_[0]
         print(f'obs_t.shape={obs_t_.shape}; obs_tp1.shape={obs_tp1_.shape}; action={action_}; rew={rew_}; terminated={terminated_}; truncated={truncated_}; info={info_}')
-        obss.append(obs_t_)
 
     atari_env = env.unwrapped
     assert isinstance(atari_env, AtariEnv)
-    meanings = atari_env.get_action_meanings()
+    meanings = atari_env.get_action_meanings() + ['SAVE']
     meaning_to_index = {meaning: i for i, meaning in enumerate(meanings)}
     key_to_index = {key: meaning_to_index[meaning] for key, meaning in KEY_TO_MEANING.items()}
     play.play(env, fps=fps, zoom=zoom, callback=callback, keys_to_action=key_to_index, noop=meaning_to_index['NOOP'])
-
-    print('Saving...')
-    pickle.dump(obss, fd)
-    fd.close()
-    print('Saved.')
 
 
 if __name__ == '__main__':
---
+++ panama_joe/wrappers.py
@@ -0,0 +1,153 @@
+import os
+import pickle
+import time
+
+import gymnasium as gym
+from gymnasium import spaces
+from shimmy.atari_env import AtariEnv
+
+
+class AtariDemo(gym.Wrapper):
+    """
+        Records actions taken, creates checkpoints, allows time travel, restoring and saving of states
+    """
+
+    def __init__(self, env, disable_time_travel=False):
+        super(AtariDemo, self).__init__(env)
+        # self.action_space = spaces.Discrete(len(env.unwrapped._action_set)+1) # add "time travel" action
+        self.action_space = spaces.Discrete(len(env.unwrapped.get_action_meanings())+1)  # add "save" action
+        self.save_every_k = 100
+        # self.max_time_travel_steps = 10000
+        # self.disable_time_travel = disable_time_travel
+
+        self.actions = None
+        self.lives = None
+        self.checkpoints = None
+        self.checkpoint_action_nr = None
+        self.obs = None
+        self.rewards = None
+        self.terminated = None
+        self.truncated = None
+        self.info = None
+
+    def step(self, action):
+        # if action >= len(self.env.unwrapped.get_action_meanings()):
+            #     if self.disable_time_travel:
+        # obs, reward, terminated, truncated, info = self.env.step(0)
+        # else:
+        #     obs, reward, terminated, truncated, info = self.time_travel()
+
+        # else:
+        # if self.steps_in_the_past > 0:
+        #     self.restore_past_state()
+
+        # if len(self.done) > 0 and self.done[-1]:
+        #     obs = self.obs[-1]
+        #     reward = 0
+        #     done = True
+        #     info = None
+
+        # else:
+        atari_env = self.env.unwrapped
+        assert isinstance(atari_env, AtariEnv)
+        self.lives.append(atari_env.ale.lives())
+
+        if action >= len(atari_env.get_action_meanings()):
+            obs, reward, terminated, truncated, info = self.env.step(0)
+            stamp = int(time.time())
+            self.save_to_file(str(stamp) + '.demo')
+        else:
+            obs, reward, terminated, truncated, info = self.env.step(action)
+            self.actions.append(action)
+            self.obs.append(obs)
+            self.rewards.append(reward)
+            self.terminated.append(terminated)
+            self.truncated.append(truncated)
+            self.info.append(info)
+
+        # periodic checkpoint saving
+        if not terminated and not truncated:
+            if (len(self.checkpoint_action_nr)>0 and len(self.actions) >= self.checkpoint_action_nr[-1] + self.save_every_k) \
+                    or (len(self.checkpoint_action_nr)==0 and len(self.actions) >= self.save_every_k):
+                self.save_checkpoint()
+
+        return obs, reward, terminated, truncated, info
+
+    def reset(self, seed=None, options=None):
+        obs = self.env.reset(seed=seed)
+        self.actions = []
+        self.lives = []
+        self.checkpoints = []
+        self.checkpoint_action_nr = []
+        self.obs = [obs]
+        self.rewards = []
+        self.terminated = [False]
+        self.truncated = [False]
+        self.info = [None]
+        # self.steps_in_the_past = 0
+        return obs
+
+    # def time_travel(self):
+    #     if len(self.obs) > 1:
+    #         reward = self.rewards.pop()
+    #         self.obs.pop()
+    #         self.done.pop()
+    #         self.info.pop()
+    #         self.lives.pop()
+    #         obs = self.obs[-1]
+    #         done = self.done[-1]
+    #         info = self.info[-1]
+    #         self.steps_in_the_past += 1
+    #
+    #     else: # reached time travel limit
+    #         reward = 0
+    #         obs = self.obs[0]
+    #         done = self.done[0]
+    #         info = self.info[0]
+    #
+    #     # rewards are differences in subsequent state values, and so should get reversed sign when going backward in time
+    #     reward = -reward
+    #
+    #     return obs, reward, done, info
+
+    def save_to_file(self, file_name):
+        dat = {'actions': self.actions, 'checkpoints': self.checkpoints, 'checkpoint_action_nr': self.checkpoint_action_nr,
+               'rewards': self.rewards, 'lives': self.lives}
+        with open(file_name, "wb") as f:
+            pickle.dump(dat, f)
+
+    # def load_from_file(self, file_name):
+    #     self.reset()
+    #     with open(file_name, "rb") as f:
+    #         dat = pickle.load(f)
+    #     self.actions = dat['actions']
+    #     self.checkpoints = dat['checkpoints']
+    #     self.checkpoint_action_nr = dat['checkpoint_action_nr']
+    #     self.rewards = dat['rewards']
+    #     self.lives = dat['lives']
+    #     self.load_state_and_walk_forward()
+    #
+    def save_checkpoint(self):
+        chk_pnt = self.env.unwrapped.clone_state()
+        self.checkpoints.append(chk_pnt)
+        self.checkpoint_action_nr.append(len(self.actions))
+
+    def restore_past_state(self):
+        self.actions = self.actions[:-self.steps_in_the_past]
+        while len(self.checkpoints)>0 and self.checkpoint_action_nr[-1]>len(self.actions):
+            self.checkpoints.pop()
+            self.checkpoint_action_nr.pop()
+        self.load_state_and_walk_forward()
+        self.steps_in_the_past = 0
+
+    def load_state_and_walk_forward(self):
+        if len(self.checkpoints)==0:
+            self.env.reset()
+            time_step = 0
+        else:
+            self.env.unwrapped.restore_state(self.checkpoints[-1])
+            time_step = self.checkpoint_action_nr[-1]
+
+        for a in self.actions[time_step:]:
+            action = self.env.unwrapped._action_set[a]
+            self.env.unwrapped.ale.act(action)
